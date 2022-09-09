c011d823db58f560b6903a43377b991186433c0b
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-08 20:43:32 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- entrypoint
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	return (0);
+}
