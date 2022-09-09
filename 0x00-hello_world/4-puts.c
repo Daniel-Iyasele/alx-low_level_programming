2b3459e76412df3523446056ea0ea12de7eed702
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-08 18:10:03 -0700

    printing with puts();

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* This program prints using a puts function */
+
+int main (void){
+	
+       puts("Programming is like building a multilingual puzzle\0\n");
+    
+return 0;
+}
+
