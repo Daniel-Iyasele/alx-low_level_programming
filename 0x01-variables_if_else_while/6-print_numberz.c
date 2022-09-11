93b0e592df46c846d1cd11248f51ee00848f97ab
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-11 15:15:57 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* This program prints figures 0-9 using ASCII coddes */
+/**
+ * main-entrypoint
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char first_no = '0', last_no = '9';
+
+	while (first_no <= last_no)
+	{
+		putchar(first_no);
+		first_no++;
+	}
+	putchar('\n');
+	return (0);
+}
