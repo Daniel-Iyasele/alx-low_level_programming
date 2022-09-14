ae8c95b486667cb5ab259302901d20f2ea543680
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-14 03:57:54 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,10 @@ int print_last_digit(int n)
 {
 	int ret;
 
-	if (n < 0)
-		n = -1 * n;
 	ret = n % 10;
+	if (ret < 0)
+		ret = -1 * ret;
+	 /*ret = n % 10;*/
 	_putchar('0' + ret);
 	return (ret);
 }
