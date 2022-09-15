1fb77f3951370792a64613cb11a9ebbd3086240c
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-15 10:24:48 -0700

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * more_numbers - function prints 0-14 ten times
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j = 0;
+
+		while (j <= 14)
+		{
+			if (j >= 10)
+			{
+				/* conversion of int greater than 9 in AScii to char */
+				int last, first;
+
+				last = j % 10;
+				first = j / 10;
+				first = first + '0';
+				last = last + '0';
+				_putchar(first);
+				_putchar(last);
+			}
+			else
+			{
+				int new;
+
+				new = j + '0';
+				_putchar(new);
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
