0ad7fd85f11f00d414554dfec15bb50bf8f8deda
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-15 14:58:27 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - entrypoint
- *
- * Author : @Daniel Iyasle @ alx
- * body: prints numbers from 1-100?
- * if number is a multiple of 3 replace with FiZZ?
- * if number is a multiple of 5 replace with Buzz?
- * if number is both a multiple of 3 & 5 replace with FizzBuzz
+ * main - entrypoint to access the program
  *
  * Return: always 0
  */
@@ -19,21 +13,34 @@ int main(void)
 	{
 		if (((i % 5) == 0) && ((i % 3) == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+			if (i < 100)
+				putchar(' ');
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			if (i < 100)
+				putchar(' ');
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
+			if (i < 100)
+				putchar(' ');
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+			if (i < 100)
+				putchar(' ');
 		}
 	}
 	putchar('\n');
 	return (0);
+	/*Author : @Daniel Iyasle @ alx */
+	/*body: prints numbers from 1-100?*/
+	/*if number is a multiple of 3 replace with FiZZ?*/
+	/*if number is a multiple of 5 replace with Buzz? */
+	/*if number is both a multiple of 3 & 5 replace with FizzBuzz */
 }
