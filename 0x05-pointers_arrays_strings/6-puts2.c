07f8400e941901996ba5503c9c9afc222fcedd1f
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-22 10:25:35 -0700

    print even numbers in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - checks if a character is divisible by 2
+ * @str:characters to be checked
+ */
+void puts2(char *str)
+{
+	char ch, *beg_ptr;
+	int i;
+
+	beg_ptr = &str[0];
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		ch = *beg_ptr;
+		if (ch % 2 == 0)
+		{
+			_putchar(ch);
+		}
+		beg_ptr++;
+	}
+	_putchar('\n');
+}
