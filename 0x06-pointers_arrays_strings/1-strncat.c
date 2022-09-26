a541d3f9bd00429a01f10731d73847d62bf719e2
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-25 18:44:02 -0700

    dynamic string concatenation

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strncat - function to concatenate string using diff lenth size for src
+ * @dest: destinattion string
+ * @src: source string
+ * @n: length of source string
+ *
+ * Return: a pointer to the destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len_of_dest, len_of_src;
+	char *ch, *beg_dest, *beg_src;
+
+	ch = dest;
+	beg_dest = dest;
+	beg_src = src;
+	i = 0;
+	while (*(dest + i) != '\0')
+	{
+		*(beg_dest + i) = *(dest + i);
+		i++;
+	}
+	len_of_dest =  i;
+	j = 0;
+	while (j < n)
+	{
+		*(beg_src + j) = *(src + j);
+		j++;
+	}
+	len_of_src = j;
+	for (i = 0; i < len_of_dest; i++)
+	{
+		*(ch + i) = *(beg_dest + i);
+	}
+	for (j = 0; j < len_of_src; j++)
+	{
+		*(ch + i + j) = *(src + j);
+	}
+	dest = ch;
+	src = beg_src;
+	return (ch);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
