8501f060fcb374b28397a742504de9edb52e1efa
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-27 06:36:28 -0700

    copy bytes from source to dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (*(src + i) && i < n)
+	while ((*(src + i) != '\0') && i < n)
 	{
 		*(dest + i) = *(src + i);
 		i++;
