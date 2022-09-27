2b52bad0559c22933734f7535c7bdf47ca292894
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-09-26 20:28:57 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - funcion ha copies n byte of words from source
+ * @dest: destination string
+ * @src: source string
+ * @n : length of byte of string to copy
+ *
+ * Return: dest pointer value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (*(src + i) && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
