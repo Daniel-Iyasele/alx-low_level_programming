a69520ca701fa387eb373859bf10d2941bc69b9f
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-10-11 11:52:13 -0700

    free heap memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - this frees struct dog
+ * @d: pointer to struct dog
+ *
+ * Ruturn: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
