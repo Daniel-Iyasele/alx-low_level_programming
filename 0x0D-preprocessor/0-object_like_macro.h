97ea70d48662c45a7e044b714fbb2c512fab7496
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-10-12 12:37:19 -0700

    macro in c

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
+#ifndef OBJECT_LIKE_MACRO_h
+#define OBJECT_LIKE_MACRO_h
 #include <stdio.h>
 #include <stdlib.h>
-#ifndef OBJECT_LIKE_MACRO_h
-#define OBJECT_LIKE_MACRO_h 
 #define SIZE 1024
-#endif /*0-OBJECT_LIKE_MACRO_h*/
+#endif/*0-OBJECT_LIKE_MACRO_h*/
