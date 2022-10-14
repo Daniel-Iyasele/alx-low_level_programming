a386aa9701d35e275018781616e1608d290d1bf9
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-10-13 18:00:08 -0700

    print int numbers in varaidic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints values in a variadic function
+ * @separator: string that separates printed values
+ * @n: number of integers passed to the function
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ptr, int));
+		if (*separator && (i < n - 1))
+			printf("%c ", *separator);
+	}
+	printf("\n");
+	va_end(ptr);
+}
