fa28211acd9a870c07e61623d29ee1862603f641
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-10-27 15:51:28 -0700

    get bit at index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ unsigned int power(unsigned int a, unsigned int b);
 int check_string(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 #endif /*main_h*/
