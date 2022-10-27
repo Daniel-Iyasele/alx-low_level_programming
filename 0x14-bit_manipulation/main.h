d46ae6f5f523210ae2523aba00bc9e5e0a82999f
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-10-27 14:57:28 -0700

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,4 +5,5 @@
 unsigned int binary_to_uint(const char *b);
 unsigned int power(unsigned int a, unsigned int b);
 int check_string(const char *b);
+void print_binary(unsigned long int n);
 #endif /*main_h*/
