2204c53b61ecc375d676ec3ab13ecad94b834489
Daniel-Iyasele <charlesiregbe007@gmail.com>
2022-10-30 15:20:01 -0700

    98 fibonacci without array

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Program that prints the first 98 fibonacci series*/
+/**
+ * main - program entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long fib, first, second, next, i;
+
+	first = 1;
+	second = 2;
+	printf("%lu, ", first);
+	printf("%lu, ", second);
+	for (i = 2; i < 98; i++)
+	{
+		next = first + second;
+		printf("%lu", next);
+		if (i < 97)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		first = second;
+		second = next;
+	}
+	putchar('\n');
+	return (0);
+}
